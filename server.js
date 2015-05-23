bc301aa38f784e7bc3d85563730b577819b239e6
Mahir Kothary <mahirk@D-108-179-129-194.dhcp4.washington.edu>
2015-05-23 14:14:45 -0700

    1.0

---
+++ server.js
@@ -0,0 +1,20 @@
+var Slack = require('slack-node');
+var express    = require('express');
+var app        = express();
+apiToken = "APITOKEN";
+
+slackapp = new Slack(apiToken);
+
+app.get('/', function (req, res) {
+  slackapp.api("channels.list", function(err, response) {
+    var channels = response.channels;
+    var namelist = [];
+    for(var i = 0; i < channels.length; i++){
+      namelist[i] = channels[i].name;
+    }
+    res.locals.place = namelist;
+    res.send("" + namelist);
+  });
+});
+
+app.listen(8080);
